bb332a3383a76999ded0713989a4317ce863d2ff
alessandro-barate <alebarat@gmail.com>
2024-06-20 15:51:20 +0100

    [UPD]

---
+++ server.php
@@ -0,0 +1,34 @@
+<?php
+
+$albums = [
+    [
+        'name' => 'The Dark Side Of The Moon',
+        'artist' => 'Pink Floyd',
+        'year' => '1973',
+    ],
+    [
+        'name' => 'Hybrid Theory',
+        'artist' => 'Linkin Park',
+        'year' => '2000',
+    ],
+    [
+        'name' => 'Thinking In Textures',
+        'artist' => 'Chet Faker',
+        'year' => '2012',
+    ],
+    [
+        'name' => 'Californication',
+        'artist' => 'Red Hot Chili Peppers',
+        'year' => '1999',
+    ],
+    [
+        'name' => 'Dookie',
+        'artist' => 'Green Day',
+        'year' => '1994',
+    ],
+    [
+        'name' => 'Fink',
+        'artist' => 'Perfect Darkness',
+        'year' => '2011',
+    ],
+];
