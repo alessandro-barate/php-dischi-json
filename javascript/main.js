2001acc8035d1e102e1169857b0b75e7c85b4a68
alessandro-barate <alebarat@gmail.com>
2024-06-20 17:42:50 +0100

    [UPD] Axios call added

--- javascript/main.js
+++ javascript/main.js
@@ -4,39 +4,19 @@ createApp({
     data(){
         return {
             title: 'dscdsc',
-            albums: [
-                {
-                    name: 'The Dark Side Of The Moon',
-                    artist: 'Pink Floyd',
-                    year: '1973',
-                },
-                {
-                    name: 'Hybrid Theory',
-                    artist: 'Linkin Park',
-                    year: '2000',
-                },
-                {
-                    name: 'Thinking In Textures',
-                    artist: 'Chet Faker',
-                    year: '2012',
-                },
-                {
-                    name: 'Californication',
-                    artist: 'Red Hot Chili Peppers',
-                    year: '1999',
-                },
-                {
-                    name: 'Dookie',
-                    artist: 'Green Day',
-                    year: '1994',
-                },
-                {
-                    name: 'Fink',
-                    artist: 'Perfect Darkness',
-                    year: '2011',
-                },
-            ],
-        }
+            albums: [],
+            urlAlbums: "http://localhost:8888/php-dischi-json/server.php",
+        };
+    },
+
+    methods: {
+        getAlbums(){
+            axios.get(this.urlAlbums).then((response) => this.albums = response.data);
+        },
+    },
+
+    created(){
+        this.getAlbums();
     }
 }).mount('#app');
 
