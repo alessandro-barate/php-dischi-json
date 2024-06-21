aa90487e35323f286de43467b4d2ee0f1436db90
alessandro-barate <alebarat@gmail.com>
2024-06-21 15:45:54 +0100

    [UPD] Some styles added

--- index.php
+++ index.php
@@ -8,11 +8,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
+    <!--  FINE font -->
+
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- FINE Bootstrap -->
 
-    <!-- FINE Css -->
+    <!-- Css -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- FINE Css -->
 
@@ -29,9 +35,9 @@
         <div class="container">
             <div class="row">
                 <div class="col d-flex flex-wrap text-center justify-content-center">
-                    <div v-for="album in albums" class="col-3 m-3">
+                    <div v-for="album in albums" class="col-3 m-4 card">
                         <div @click="">
-                            <img class="cover" :src="album.cover" alt="">
+                            <img class="cover p-2" :src="album.cover" alt="">
                             <h4>{{ album.name }}</h4>
                             <h5>{{ album.artist }}</h5>
                             <h5>{{ album.year }}</h5>
